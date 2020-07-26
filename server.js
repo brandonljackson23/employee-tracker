f4bdfa114bc062342a56c713bc28519e63960c0d
Brandon Jackson <brandonljackson23@gmail.com>
2020-07-26 11:18:29 -0600

    add employee prompts and routes

--- server.js
+++ server.js
@@ -2,67 +2,237 @@ const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const connection = require('./db/database');
 
+// Display all deparments
+const allDepartments = () => {
+  const departmentsList = [];
+  connection.promise().query('SELECT * FROM departments;', function (err, res) {
+    if (err) { throw err }
+    for (var i = 0; i < res.length; i++) {
+      departmentsList.push(res[i].name);
+    }
+  })
+  return departmentsList;
+};
+
+// Display all roles
+const allRoles = () => {
+  const rolesList = [];
+  connection.promise().query(`SELECT * FROM roles;`, function (err, res) {
+    if (err) { throw err }
+    for (var i = 0; i < res.length; i++) {
+      rolesList.push(res[i].title);
+    }
+  })
+  return rolesList;
+};
+
+// Display all managers
+function allEmployees() {
+  const managersList = [];
+  connection.promise().query(`SELECT * FROM employees;`, function (err, res) {
+    if (err) { throw err }
+    for (var i = 0; i < res.length; i++) {
+      let name = (res[i].first_name).concat(' ').concat(res[i].last_name)
+      managersList.push(name);
+    }
+  })
+  return managersList;
+};
+
 // Display main menu
 const mainMenu = () => {
   inquirer.prompt(
-      {
-          type: "list",
-          name: "main_menu",
-          message: "What would you like to do?",
-          choices: [
-              "View all employees",
-              "Add an employee",
-              "Update an employee's role",
-              "View all roles",
-              "Add a role",
-              "View all departments",
-              "Add a department",
-              "Exit"
-          ]
-      }
+    {
+      type: "list",
+      name: "main_menu",
+      message: "What would you like to do?",
+      choices: [
+          "View all employees",
+          "Add an employee",
+          "Update an employee's role",
+          "View all roles",
+          "Add a role",
+          "View all departments",
+          "Add a department",
+          "Exit"
+      ]
+    }
   )
   .then(response => {
-      if (response.main_menu === "View all employees") {
-          return viewEmployees()
-      } else if (response.main_menu === "Add an employee") {
-          return createEmployee();
-      } else if (response.main_menu === "Update an employee's role") {
-          return updateEmployee();
-      } else if (response.main_menu === "View all roles") {
-          return viewRoles();
-      } else if (response.main_menu === "Add a role") {
-          return createRole();
-      } else if (response.main_menu === "View all departments") {
-          return viewDepartments();
-      } else if (response.main_menu === "Add a department") {
-          return createDepartment();
-      } else if (response.main_menu === "Exit") {
-          return exitMenu();
+    if (response.main_menu === "View all employees") {
+        return viewEmployees()
+    } else if (response.main_menu === "Add an employee") {
+        return createEmployee();
+    } else if (response.main_menu === "Update an employee's role") {
+        return updateEmployee();
+    } else if (response.main_menu === "View all roles") {
+        return viewRoles();
+    } else if (response.main_menu === "Add a role") {
+        return createRole();
+    } else if (response.main_menu === "View all departments") {
+        return viewDepartments();
+    } else if (response.main_menu === "Add a department") {
+        return createDepartment();
+    } else if (response.main_menu === "Exit") {
+        return exitMenu();
+    }
+  })
+};
+
+const viewEmployees = () => {
+  connection.promise().query(`
+    SELECT employees.id AS ID,
+    CONCAT(employees.first_name, ' ', employees.last_name) AS Name,
+    roles.title AS Role,
+    roles.salary AS Salary,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS Manager,
+    departments.name AS Department
+    FROM employees 
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN employees manager ON employees.manager_id = manager.id;`
+  )
+  .then(([results]) => {
+      console.table(results);
+      return mainMenu();
+  })
+};
+
+// Create an employee
+const createEmployee = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is the employee's first name?",
+      validate: first_name => {
+          if (first_name) {
+              return true;
+          } else {
+              console.log('Please enter a first name.');
+              return false;
+          }
       }
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is the new employee's last name?",
+      validate: last_name => {
+          if (last_name) {
+              return true;
+          } else {
+              console.log('Please enter a last name.');
+              return false;
+          }
+      }
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is the employee's title?",
+      choices: allRoles()
+    },
+    {
+      type: "list",
+      name: "manager",
+      message: "Who is the employee's manager?",
+      choices: allEmployees()
+    }
+  ])
+  .then(response => {
+    const employeeFirstName = response.first_name;
+    const employeeLastName = response.last_name;
+    const employeeRole = response.role;
+    const employeeManager = response.manager;
+    var roleId = '';
+    var managerId = '';
+
+    connection.promise().query(`SELECT * FROM roles;`, function (err, res) {
+      if (err) { throw err };
+      for (var i = 0; i < res.length; i++) {
+        if (employeeRole === res[i].title) {
+            roleId = (res[i].id);
+        }
+      }
+    })
+    connection.promise().query(`SELECT * FROM employees;`, function (err, result) {
+      if (err) { throw err };
+      for (var i = 0; i < result.length; i++) {
+        let resManager = (result[i].first_name).concat(' ').concat(result[i].last_name)
+        if (employeeManager === resManager) {
+            managerId = (result[i].id);
+        }
+      }
+      let post = {
+        first_name: employeeFirstName,
+        last_name: employeeLastName,
+        role_id: roleId,
+        manager_id: managerId
+    }
+    connection.query(`INSERT INTO employees SET ?`, post);
+    console.log(`
+              
+`+ post.first_name + " " + post.last_name + ` was added.
+
+    `
+    );
+    return mainMenu();
+    })
+  })
+};
+
+// Update employee's role
+const updateEmployee = () => {
+  return inquirer.prompt([
+    { 
+        type: "list",
+        name: "role",
+        message: "What is the employee's title?",  
+        choices: allRoles()          
+    }
+  ])
+  .then(response => {
+    if (response.role === "Role"){
+      let targetEmployee = response.updateEmployeeID;
+      let newRole = response.updateEmployeeRole;
+      var roleID = '';
+      connection.promise().query(`SELECT * FROM roles;`, function (err, res) {
+        if (err) {throw err};
+        for (var i = 0; i < res.length; i++) {
+          if (newRole === res[i].title){
+              roleID = (res[i].id);
+          }
+        }
+        connection.query(`UPDATE employees SET employees.role_id = ? WHERE CONCAT(employees.first_name, ' ', employees.last_name) = ?;`, [roleID, targetEmployee]);
+        console.log("You updated " + targetEmployee + "'s role to " + newRole);
+        return mainMenu();
+      })
+    }
   })
 };
 
 console.log(
-  `
-   _______________________________________________________
-  |                                                       |
-  |                                                       |
-  |     _____                 _                           |
-  |    | ____|_ __ ____ _ ___| | ___  _   _  ___  ___     |
-  |    |  _| | '_ ' _  | '_  | |/ _  | | | |/ _ |/ _ |    |
-  |    | |___| | | | | | |_) | | (_) | |_| |  __/  __/    |
-  |    |_____|_| |_| |_| ,__/|_|____/|___, |____|____|    |
-  |                    |_|           |_____/              |
-  |        __  __                                         |
-  |       |   /  | __ _ _ ___  __ _  __ _  ___ _ __       |
-  |       | | /| |/ _' | '_  |/ _' |/ _' |/ _ | '__|      |
-  |       | |  | | (_) | | | | (_) | (_) |  __/ |         |
-  |       |_|  |_|___,_|_| |_|___,_|___, |____|_|         |
-  |                                 |____/                |
-  |                                                       |
-  |_______________________________________________________|
+`
+  _______________________________________________________
+|                                                       |
+|                                                       |
+|     _____                 _                           |
+|    | ____|_ __ ____ _ ___| | ___  _   _  ___  ___     |
+|    |  _| | '_ ' _  | '_  | |/ _  | | | |/ _ |/ _ |    |
+|    | |___| | | | | | |_) | | (_) | |_| |  __/  __/    |
+|    |_____|_| |_| |_| ,__/|_|____/|___, |____|____|    |
+|                    |_|           |_____/              |
+|        __  __                                         |
+|       |   /  | __ _ _ ___  __ _  __ _  ___ _ __       |
+|       | | /| |/ _' | '_  |/ _' |/ _' |/ _ | '__|      |
+|       | |  | | (_) | | | | (_) | (_) |  __/ |         |
+|       |_|  |_|___,_|_| |_|___,_|___, |____|_|         |
+|                                 |____/                |
+|                                                       |
+|_______________________________________________________|
   
-  `
-  );
+`
+);
   
-  mainMenu();
+mainMenu();
